b250d18818e6f2ae17b292690b3d23ee81e9c6bf
Dan Printzell <xwildn00bx@gmail.com>
2018-10-08 01:37:13 +0200

    Fixed unittest/insecure.sh :mad:
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- unittest/insecure.sh
+++ unittest/insecure.sh
@@ -5,37 +5,37 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-OUTPUT="$(grep -Rn strcpy $*)"
+OUTPUT="$(grep -Rn "\<strcpy\>" $*)"
 if [[ "$OUTPUT" ]]; then
 		OUTPUT="strcpy:\n${OUTPUT}"
 fi
 
-OUTPUT2="$(grep -Rn strcat $*)"
+OUTPUT2="$(grep -Rn "\<strcat\>" $*)"
 if [[ "$OUTPUT2" ]]; then
 		OUTPUT+="\n\nstrcat:\n${OUTPUT2}"
 fi
 
-OUTPUT3="$(grep -Rn sprintf $*)"
+OUTPUT3="$(grep -Rn "\<sprintf\>" $*)"
 if [[ "$OUTPUT3" ]]; then
 		OUTPUT+="\n\nsprintf:\n${OUTPUT3}"
 fi
 
-OUTPUT4="$(grep -Rn gets $*)"
+OUTPUT4="$(grep -Rn "\<gets\>" $*)"
 if [[ "$OUTPUT4" ]]; then
 		OUTPUT+="\n\ngets:\n${OUTPUT4}"
 fi
 
-OUTPUT5="$(grep -Rn strtok $*)"
+OUTPUT5="$(grep -Rn "\<strtok\>" $*)"
 if [[ "$OUTPUT5" ]]; then
 		OUTPUT+="\n\nstrtok:\n${OUTPUT5}"
 fi
 
-OUTPUT6="$(grep -Rn scanf $*)"
+OUTPUT6="$(grep -Rn "\<scanf\>" $*)"
 if [[ "$OUTPUT6" ]]; then
 		OUTPUT+="\n\nscanf:\n${OUTPUT6}"
 fi
 
-OUTPUT7="$(grep -Rn itoa $*)"
+OUTPUT7="$(grep -Rn "\<itoa\>" $*)"
 if [[ "$OUTPUT7" ]]; then
 		OUTPUT+="\n\nitoa:\n${OUTPUT7}"
 fi
